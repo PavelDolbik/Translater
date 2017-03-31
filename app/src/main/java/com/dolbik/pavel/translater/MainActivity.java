0dd44055207f8a0fab1c7cc3dcc29bcec38a34dd
Pavel Dolbik <pasha656@tut.by>
2017-03-31 15:48:27 +0300

    Add BottomNavigationView

--- app/src/main/java/com/dolbik/pavel/translater/MainActivity.java
+++ app/src/main/java/com/dolbik/pavel/translater/MainActivity.java
@@ -1,8 +1,12 @@
 package com.dolbik.pavel.translater;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_translate:
+                    Log.d("Pasha", "Translate");
+                    return true;
+                case R.id.navigation_favorite:
+                    Log.d("Pasha", "Favorite");
+                    return true;
+            }
+            return false;
+        }
+    };
 
 
     @Override
