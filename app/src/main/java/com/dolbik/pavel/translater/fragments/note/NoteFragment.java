98953fb90118fd08ca8cbe2052d5aa0bb0f5b44c
Pavel Dolbik <pasha656@tut.by>
2017-04-03 13:30:41 +0300

    Add tabLayout

--- app/src/main/java/com/dolbik/pavel/translater/fragments/note/NoteFragment.java
+++ app/src/main/java/com/dolbik/pavel/translater/fragments/note/NoteFragment.java
@@ -4,8 +4,11 @@ package com.dolbik.pavel.translater.fragments.note;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,11 +24,24 @@ public class NoteFragment extends Fragment {
     private ViewPager viewPager;
 
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setShowHideAnimationEnabled(false);
+            actionBar.hide();
+        }
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_note, container, false);
         viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        TabLayout tabs = (TabLayout) view.findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
         setupViewPager();
         return view;
     }
--- app/src/main/java/com/dolbik/pavel/translater/fragments/translate/TranslateFragment.java
+++ app/src/main/java/com/dolbik/pavel/translater/fragments/translate/TranslateFragment.java
@@ -4,6 +4,8 @@ package com.dolbik.pavel.translater.fragments.translate;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,17 @@ import android.view.ViewGroup;
 import com.dolbik.pavel.translater.R;
 
 public class TranslateFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setShowHideAnimationEnabled(false);
+            actionBar.show();
+        }
+    }
 
 
     @Nullable
