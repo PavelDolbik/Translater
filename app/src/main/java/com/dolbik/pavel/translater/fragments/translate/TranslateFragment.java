bcbee47d2de67a3030bdea29b4ac8290336b9857
Pavel Dolbik <pasha656@tut.by>
2017-04-03 17:35:09 +0300

    Add custom tool bar

--- app/src/main/java/com/dolbik/pavel/translater/fragments/translate/TranslateFragment.java
+++ app/src/main/java/com/dolbik/pavel/translater/fragments/translate/TranslateFragment.java
@@ -4,8 +4,8 @@ package com.dolbik.pavel.translater.fragments.translate;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,22 +15,15 @@ import com.dolbik.pavel.translater.R;
 public class TranslateFragment extends Fragment {
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setShowHideAnimationEnabled(false);
-            actionBar.setDisplayShowTitleEnabled(false);
-            actionBar.show();
-        }
-    }
-
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_translate, container, false);
+        View view = inflater.inflate(R.layout.fragment_translate, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        return view;
     }
 
 
