db21471998f24b96e44d99ce7d66cedf777595a2
Pavel Dolbik <pasha656@tut.by>
2017-04-04 17:43:42 +0300

    Add init langs json

--- app/src/main/java/com/dolbik/pavel/translater/fragments/translate/TranslatePresenter.java
+++ app/src/main/java/com/dolbik/pavel/translater/fragments/translate/TranslatePresenter.java
@@ -7,7 +7,14 @@ import com.arellomobile.mvp.MvpPresenter;
 import com.dolbik.pavel.translater.TApplication;
 import com.dolbik.pavel.translater.model.Langs;
 import com.dolbik.pavel.translater.utils.DataRepository;
+import com.google.gson.JsonObject;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 import java.util.Locale;
 
 import rx.SingleSubscriber;
@@ -25,7 +32,9 @@ public class TranslatePresenter extends MvpPresenter<TranslateView> {
         Log.d("Pasha", "onFirstViewAttach "+ Locale.getDefault().getLanguage());
         Log.d("Pasha", "Is connected "+getApplication().isConnected());
 
-        new DataRepository().getAllLangs(Locale.getDefault().getLanguage())
+        loadInitLangsJson();
+
+        /*new DataRepository().getAllLangs(Locale.getDefault().getLanguage())
                 .subscribe(new SingleSubscriber<Langs>() {
                     @Override
                     public void onSuccess(Langs value) {
@@ -38,9 +47,34 @@ public class TranslatePresenter extends MvpPresenter<TranslateView> {
                     public void onError(Throwable error) {
                         error.printStackTrace();
                     }
-                });
+                });*/
 
     }
+
+
+    private void loadInitLangsJson() {
+        try {
+            InputStream is = getApplication().getAssets().open("init_langs.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+
+            JSONObject obj = new JSONObject(json);
+            JSONObject langs = obj.getJSONObject("langs");
+
+            Iterator<?> keys = langs.keys();
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                Log.d("Pasha", ""+key+" "+langs.get(key));
+            }
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     private TApplication getApplication() {
